f84e974f724ff66853fd0f95317ef78ff90d8cd0
Ivan <ivansalnikov90@gmail.com>
2024-02-13 12:29:35 +0300

    first commit

---
+++ import json.py
@@ -0,0 +1,6 @@
+# Импорт библиотек и модулей: мы начинаем с импорта необходимых модулей Python, таких как `json`,
+# `os` для работы с файлами и `datetime` для работы с датами и временем.
+
+import json
+import os
+from datetime import datetime
